213e2458d2a38006acce52fd4b33e1de1093cdc7
Philip Goos <philip.goos@campus.tu-berlin.de>
2016-07-20 01:03:05 +0200

    go forward implementiert

--- scripts/modules/BrowserActionsModule.js
+++ scripts/modules/BrowserActionsModule.js
@@ -3,10 +3,11 @@
 */
 addModule(new Module("BrowserActionsModule", function() {
 
-	var backs = [];
-/**
- * opens new tab
-*/
+	var pages = [];
+	var currPage = 0;
+	/**
+ 	 * opens new tab
+	 */
 	var newTab = new Action(0, globalCommonState);
 	newTab.addCommand(new Command("new tab", 0));
 	newTab.act = function() {
@@ -14,20 +15,22 @@ addModule(new Module("BrowserActionsModule", function() {
 	};
 	this.addAction(newTab);
 
-/**
- * opens new page
-*/	
+	/**
+	 * opens new page
+	 */
 	var openPage = new Action(1, globalCommonState);
 	openPage.addCommand(new Command("open (.*)", 1));
 	openPage.act = function(arguments) {
 		var url = "";
 		var repWhitespace = "";
+		//opens new page in new tab
 		if(arguments[0].search(/new tab/) != -1) {
 			// opens page in new tab
 			repWhitespace = arguments[0].replace(/\s/g, '');
 			var split = repWhitespace.split("innewtab", 1);
 			url = "http://www." + split + ".com";
-			chrome.tabs.create({url: url, active: true});	
+			chrome.tabs.create({url: url, active: true});
+		//opens new page in current tab
 		} else {
 			//opens page in current tab
 			repWhitespace = arguments[0].replace(/\s/g, '');
@@ -37,9 +40,9 @@ addModule(new Module("BrowserActionsModule", function() {
 	};
 	this.addAction(openPage);
 
-/**
- * closes tab(s)/window/panel
-*/
+	/**
+ 	 * closes tab(s)/window/panel
+	 */
 	
 	var close = new Action(1, globalCommonState);
 	close.addCommand(new Command("close (.*)", 1));
@@ -58,9 +61,9 @@ addModule(new Module("BrowserActionsModule", function() {
 	};
 	this.addAction(close);
 
-/**
- * reloads the current tab
-*/
+	/**
+ 	 * reloads the current tab
+	 */
 	var reload = new Action(0, globalCommonState);
 	reload.addCommand(new Command("reload", 0));
 	reload.act = function() {
@@ -68,38 +71,58 @@ addModule(new Module("BrowserActionsModule", function() {
 	};
 	this.addAction(reload);
 
-/**
- * opens last visited page
-*/
-	var lastVisited = new Action(0, globalCommonState);
-	lastVisited.addCommand(new Command("go back", 0));
-	lastVisited.act = function() {
-		var test = true;
+	/**
+ 	 * goes back one page
+	 */
+	var goBack = new Action(0, globalCommonState);
+	goBack.addCommand(new Command("go back", 0));
+	goBack.act = function() {
 		notify("go back one page");
+		var test = true;
 		chrome.history.search({text: ''}, function(historyItem) {
-			backs.push(historyItem[0].url);
+			//store first historyItem in backs
+			pages.push(historyItem[0].url);
+			//checks for every page in the history which was the last visited
 			for(var i = 1; i < historyItem.length; i++) {
 				test = true;
-				for(var h = 0; h < backs.length; h++) {
-					console.log(backs.length);
-					console.log(backs[h]);
-					if (historyItem[i].url === backs[h]) {
+				for(var h = 0; h < pages.length; h++) {
+					if (historyItem[i].url === pages[h]) {
 						test = false;
 					}
 				}
+				//if current page isn't in backs, push it to backs, update current tab, save current i of page
 				if(test === true) {
-					backs.push(historyItem[i].url);
+					pages.push(historyItem[i].url);
 					chrome.tabs.update({url: historyItem[i].url, active: true});
+					currPage = pages.length - 1;
 					return;
 				}
 			}
 		});
 	};
-	this.addAction(lastVisited);
+	this.addAction(goBack);
 
-/**
- * search for an expression
-*/
+	/**
+	 * goes forward one page
+     */
+	var goForward = new Action(0, globalCommonState);
+	goForward.addCommand(new Command("go forward", 0));
+	goForward.act = function() {
+		notify("go forward one page");
+		//updates tab, deletes currentPage in pages, set currPage
+		if(currPage >= 0) {
+			chrome.tabs.update({url: pages[currPage - 1], active: true});
+			pages.slice(currPage);
+			currPage -= 2;
+		} else {
+			alert("going forward isn't possible");
+		}
+	};
+	this.addAction(goForward);
+
+	/**
+ 	 * search for an expression
+	 */
 	var search = new Action(1, globalCommonState);
 	search.addCommand(new Command("search for (.*)", 1));
 	search.act = function(arguments) {
