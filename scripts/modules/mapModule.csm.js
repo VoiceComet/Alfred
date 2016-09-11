1081af33543d125d6bfe6de10fb10d82168a5807
Chris <cech12@gmail.com>
2016-09-12 01:00:11 +0200

    - ach Ewigkeiten Rumprobieren komm ich nun an die map ran - Interaktionsfunktionen vorbereitet aber noch nicht implementeirt

--- scripts/modules/mapModule.csm.js
+++ scripts/modules/mapModule.csm.js
@@ -1,4 +1,14 @@
 /**
+ * runs a given function on front page, you can use the variable map here
+ * @param {Function} method
+ */
+function runMethodOnPage(method) {
+	var script = document.createElement('script');
+	script.appendChild(document.createTextNode('('+ method +')();'));
+	(document.body || document.head || document.documentElement).appendChild(script);
+}
+
+/**
  * show map in panel
  */
 addContentScriptMethod(
@@ -7,18 +17,74 @@ addContentScriptMethod(
         panelParams.time = 0;
         panelParams.cancelable = false;
         panelParams.fullHeight = true;
-        panelParams.html = '<div id="map" style="height:100%"></div>' +
-                '<script type="text/javascript">' +
-                'var map; ' +
-                'function initMap() { ' +
-                    'map = new google.maps.Map(document.getElementById("map"), { ' +
-                        'center: {lat: -34.397, lng: 150.644}, ' +
-                        'zoom: 8 ' +
-                    '}); ' +
-                '}'+
-                '</script>' +
-                '<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAD-XJsCGm_N1cAfYeuTwgsiFp0iWgcAi0&callback=initMap"></script>';
+
+		//add map div and needed javascript to front page
+		panelParams.html = '<div id="map" style="height:100%"></div>' +
+			'<script type="text/javascript">' +
+			'var map; ' +
+			'function initMap() { ' +
+				'map = new google.maps.Map(document.getElementById("map"), { ' +
+					'center: {lat: -34.397, lng: 150.644}, ' +
+					'zoom: 8 ' +
+				'}); ' +
+			'}'+
+			'</script>' +
+			'<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAD-XJsCGm_N1cAfYeuTwgsiFp0iWgcAi0&callback=initMap"></script>';
 
         showPanel(panelParams);
+    })
+);
+
+addContentScriptMethod(
+	new ContentScriptMethod("mapZoomIn", function() {
+		runMethodOnPage(function() {
+			console.log(map);
+			//TODO
+		});
+	})
+);
+
+addContentScriptMethod(
+	new ContentScriptMethod("mapZoomOut", function() {
+		runMethodOnPage(function() {
+			console.log(map);
+			//TODO
+		});
+	})
+);
+
+addContentScriptMethod(
+    new ContentScriptMethod("mapScrollUp", function() {
+		runMethodOnPage(function() {
+			console.log(map);
+			//TODO
+		});
+    })
+);
+
+addContentScriptMethod(
+    new ContentScriptMethod("mapScrollDown", function() {
+		runMethodOnPage(function() {
+			console.log(map);
+			//TODO
+		});
+    })
+);
+
+addContentScriptMethod(
+    new ContentScriptMethod("mapScrollLeft", function() {
+		runMethodOnPage(function() {
+			console.log(map);
+			//TODO
+		});
+    })
+);
+
+addContentScriptMethod(
+    new ContentScriptMethod("mapScrollRight", function() {
+		runMethodOnPage(function() {
+			console.log(map);
+			//TODO
+		});
     })
 );
--- scripts/modules/mapModule.js
+++ scripts/modules/mapModule.js
@@ -10,6 +10,10 @@ addModule(new Module("mapModule", function () {
 		};
 	};
 
+	/**
+	 * open map in panel
+	 * @type {Action}
+	 */
 	var openMap = new Action("openMap", 0, mapState);
 	openMap.addCommand(new Command("show map", 0));
 	openMap.act = function () {
@@ -17,4 +21,69 @@ addModule(new Module("mapModule", function () {
 	};
 	this.addAction(openMap);
 
+	/**
+	 * Zoom in
+	 * @type {Action}
+	 */
+	var mapZoomIn = new Action("mapZoomIn", 0, mapState);
+	mapZoomIn.addCommand(new Command("zoom in", 0));
+	mapZoomIn.act = function () {
+		callContentScriptMethod("mapZoomIn", {});
+	};
+	mapState.addAction(mapZoomIn);
+
+	/**
+	 * Zoom out
+	 * @type {Action}
+	 */
+	var mapZoomOut = new Action("mapZoomOut", 0, mapState);
+	mapZoomOut.addCommand(new Command("zoom out", 0));
+	mapZoomOut.act = function () {
+		callContentScriptMethod("mapZoomOut", {});
+	};
+	mapState.addAction(mapZoomOut);
+
+	/**
+	 * scroll up
+	 * @type {Action}
+	 */
+	var mapScrollUp = new Action("mapScrollUp", 0, mapState);
+	mapScrollUp.addCommand(new Command("scroll up", 0));
+	mapScrollUp.act = function () {
+		callContentScriptMethod("mapScrollUp", {});
+	};
+	mapState.addAction(mapScrollUp);
+
+	/**
+	 * scroll down
+	 * @type {Action}
+	 */
+	var mapScrollDown = new Action("mapScrollDown", 0, mapState);
+	mapScrollDown.addCommand(new Command("scroll down", 0));
+	mapScrollDown.act = function () {
+		callContentScriptMethod("mapScrollDown", {});
+	};
+	mapState.addAction(mapScrollDown);
+
+	/**
+	 * scroll left
+	 * @type {Action}
+	 */
+	var mapScrollLeft = new Action("mapScrollLeft", 0, mapState);
+	mapScrollLeft.addCommand(new Command("scroll left", 0));
+	mapScrollLeft.act = function () {
+		callContentScriptMethod("mapScrollLeft", {});
+	};
+	mapState.addAction(mapScrollLeft);
+
+	/**
+	 * scroll right
+	 * @type {Action}
+	 */
+	var mapScrollRight = new Action("mapScrollRight", 0, mapState);
+	mapScrollRight.addCommand(new Command("scroll right", 0));
+	mapScrollRight.act = function () {
+		callContentScriptMethod("mapScrollRight", {});
+	};
+	mapState.addAction(mapScrollRight);
 }));
