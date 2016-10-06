00666334bc7fe75162120da1a16f8651faaa97bd
philip-goos <Berlin2008>
2016-10-07 00:18:44 +0200

    animiertes design

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -179,7 +179,7 @@ function showPanel(params) {
 function hidePanel() {
 	$("#ChromeSpeechControlPanel").animate({
 		opacity: 0, // animate slideUp
-		marginLeft: '-200px'},
+		marginLeft: '-410px'},
 		'500',
 		'linear'
 	);
